aa85aa5b46bf588bd6ae6c8ae50b6812712d20f3
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-04-13 11:55:49 +0200

    initial commit

---
+++ src/MainLoad.hx
@@ -0,0 +1,109 @@
+package;
+
+import js.html.Element;
+import js.Browser.*;
+import js.html.XMLHttpRequest;
+
+class MainLoad {
+	var req = new XMLHttpRequest();
+	var dataAtr = "data-load";
+	var loadingArr:Array<LoadObj> = [];
+	var loadingId = 0;
+
+	// modern solution
+	// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+	public function new() {
+		document.addEventListener('DOMContentLoaded', (event) -> {
+			trace('load');
+			init();
+		});
+	}
+
+	function init() {
+		var arr = document.querySelectorAll('[${dataAtr}]');
+		for (i in 0...arr.length) {
+			var wrapper:Element = cast arr[i];
+			var url = wrapper.getAttribute(dataAtr);
+			trace(url);
+			loadingArr.push({
+				el: wrapper,
+				url: url,
+			});
+		}
+		startLoading(loadingId);
+	}
+
+	function startLoading(nr:Int) {
+		if (nr >= loadingArr.length)
+			return;
+		var obj = loadingArr[nr];
+		loadHTML(obj.url, obj.el);
+		loadingId++;
+	}
+
+	function loadHTML(url:String, el:js.html.Element) {
+		req.open('GET', url);
+		req.onload = function() {
+			// trace(req.response);
+			var body = getBody(req.response);
+			// trace(body);
+			if (body == "")
+				body = req.response;
+
+			// inject code
+			processHTML(body, el);
+			// load the next
+			startLoading(loadingId);
+		};
+
+		req.onerror = function(error) {
+			console.error('[JS] error: $error');
+		};
+
+		req.send();
+	}
+
+	/**
+	 * extract the body (from documents that have a body)
+	 *
+	 * @param html
+	 */
+	function getBody(html) {
+		var test:String = html.toLowerCase(); // to eliminate case sensitivity
+		var x:Int = test.indexOf("<body");
+		if (x == -1)
+			return "";
+
+		x = test.indexOf(">", x);
+		if (x == -1)
+			return "";
+		var y = test.lastIndexOf("</body>");
+		if (y == -1)
+			y = test.lastIndexOf("</html>");
+		if (y == -1)
+			y = html.length; // If no HTML then just grab everything till end
+		return html.slice(x + 1, y);
+	}
+
+	function processHTML(content:String, target:js.html.Element) {
+		target.outerHTML = content;
+	}
+
+	function htmlToElement(html:String) {
+		var template = document.createDivElement();
+		html = untyped html.trim(); // Never return a text node of whitespace as the result
+		template.innerHTML = html;
+		return template.firstChild;
+	}
+
+	static public function main() {
+		var app = new MainLoad();
+	}
+}
+
+typedef LoadObj = {
+	@:optional var _id:String;
+	var url:String;
+	var el:Element;
+}
